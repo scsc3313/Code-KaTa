8eec3302591fa34617137e21f3eb0ba9fb8eb6cd
HSH <scsc3313@naver.com>
2016-06-15 13:19:58 +0900

    Random 객체

--- math/src/com/company/project2/CarryCount2.java
+++ math/src/com/company/project2/CarryCount2.java
@@ -1,5 +1,7 @@
 package com.company.project2;
 
+import java.util.Random;
+
 /**
  * Created by HSH on 16. 6. 14..
  */
@@ -11,23 +13,27 @@ public class CarryCount2 {
     }
 
     private void control() {
-//
-        int num1 = (int) (Math.random() * 10000);
-        int num2 = (int) (Math.random() * 10000);
+
+        int totalCount = 0;
+
+        Random random = new Random();
+        int num1 = random.nextInt(10000);
+        int num2 = random.nextInt(10000);
+
         System.out.println(num1 + " " + num2);
+
         int carry = 0;
-        int count = 0;
         while (num1 > 1 && num2 > 1) {
-            if (num1 % 10 + num2 % 10 + carry > 9){
+            if (num1 % 10 + num2 % 10 + carry > 9) { //각 자리수를 더하고 캐리 값을 더해 10보다 크면 자리올림이 일어난 것!
                 carry = 1;
-                count++;
+                totalCount++;
+            } else {
+                carry = 0; // 자리올림이 일어나지 않으면 carry는 0
             }
-            else
-                carry = 0;
             num1 /= 10;
             num2 /= 10;
         }
 
-        System.out.print("Count : "+ count);
+        System.out.print("Count : " + totalCount);
     }
 }
